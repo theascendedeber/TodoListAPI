55672eb0814971045dae58e9425bbb4ef868fcdf
theascendedeber <theascendedeber@mail.ru>
2024-10-24 14:12:08 +0300

    Generating JWT

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,14 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password []byte) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
@@ -15,4 +23,23 @@ func PasswordCheck(password, hashPassword []byte) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func GenerateJWT(id_user int, name_user string) string {
+	secret := "some_secret123"
+
+	header := "{\"alg\": \"HS256\", \"typ\": \"JWT\"}"
+	encodeHeader := base64.RawURLEncoding.EncodeToString([]byte(header))
+
+	iat := time.Now().Unix()
+	payload := fmt.Sprintf("{\"sub\":%d, \"name\":%s, \"iat\":%d}", id_user, name_user, iat)
+	encodedPayload := base64.RawURLEncoding.EncodeToString([]byte(payload))
+
+	data := fmt.Sprintf("%s.%s", encodeHeader, encodedPayload)
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(data))
+	signature := base64.RawURLEncoding.EncodeToString(h.Sum(nil))
+
+	token := fmt.Sprintf("%s.%s.%s", encodeHeader, encodedPayload, signature)
+	return token
 }
