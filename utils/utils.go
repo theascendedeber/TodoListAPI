cb5998856a888bfc6235692e77d7e0c5efb72fbb
theascendedeber <theascendedeber@mail.ru>
2024-10-24 11:48:41 +0300

    Working with hashes

---
+++ utils/utils.go
@@ -0,0 +1,18 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password []byte) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	return string(hashedPassword), err
+}
+
+func PasswordCheck(password, hashPassword []byte) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(hashPassword, password)
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
