551db3574bb8c835c9efb7e93aca4f730b34461d
theascendedeber <theascendedeber@mail.ru>
2024-10-25 16:29:37 +0300

    Refinement func RegisterUser

--- handlers/auth.go
+++ handlers/auth.go
@@ -8,20 +8,29 @@ import (
 )
 
 const (
-	requestCreateUser = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id"
+	requestCreateUser     = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id"
+	requestGetUserByEmail = "SELECT COUNT(*) FROM users WHERE email = $1"
 )
 
 func RegisterUser(c *fiber.Ctx) error {
-	var id int
+	var id, count int
 	user := new(models.User)
 
 	if err := c.BodyParser(user); err != nil {
 		return c.Status(400).SendString("Неверный формат запроса")
 	}
 
+	err := database.DB.QueryRow(requestGetUserByEmail, user.Email).Scan(&count)
+	if err != nil {
+		return c.Status(500).SendString("Ошибка при проверке существования пользователя")
+	}
+	if count > 0 {
+		return c.Status(409).SendString("Пользователь с таким Email уже существует")
+	}
+
 	hashPassword, err := utils.HashPassword([]byte(user.Password))
 	if err != nil {
-		c.Status(500).SendString("Ошибка при генерации хеша пароля")
+		return c.Status(500).SendString("Ошибка при генерации хеша пароля")
 	}
 
 	err = database.DB.QueryRow(requestCreateUser, user.Name, user.Email, hashPassword).Scan(&id)
--- routes/auth.go
+++ routes/auth.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/theascendedeber/TodoListAPI/handlers"
 )
 
 func RegisterAuthRoutes(app *fiber.App) {
 	auth := app.Group("/auth")
 
 	auth.Post("/register", handlers.RegisterUser)
-	auth.Post("/login", handlers.LoginUser)
+	// auth.Post("/login", handlers.LoginUser)
 }
